2130daf2f3d7d20e7f807a5ab743facb8ae2726a
Lingesuwaran <46320222+Lingesuwaran@users.noreply.github.com>
2020-09-08 19:01:23 +0530

    Update s.sh

--- s.sh
+++ s.sh
@@ -1,7 +1,7 @@
 LA="make"
 sudo apt install $LA
 
-echo "wget https://golang.org/dl/go1.15.1.linux-amd64.tar.gz"
-'tar -C /home/$USER -xzf go1.15.1.linux-amd64.tar.gz'
-'export PATH=$PATH:/home/$USER/go/bin'
-'go version'
+wget https://golang.org/dl/go1.15.1.linux-amd64.tar.gz
+tar -C /home/$USER -xzf go1.15.1.linux-amd64.tar.gz
+export PATH=$PATH:/home/$USER/go/bin
+go version
