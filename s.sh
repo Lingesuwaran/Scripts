c1522275c2730c92f8e07276bb00358d76fa8606
Lingesuwaran <46320222+Lingesuwaran@users.noreply.github.com>
2020-09-08 19:02:47 +0530

    Update s.sh

--- s.sh
+++ s.sh
@@ -5,3 +5,6 @@ wget https://golang.org/dl/go1.15.1.linux-amd64.tar.gz
 tar -C /home/$USER -xzf go1.15.1.linux-amd64.tar.gz
 export PATH=$PATH:/home/$USER/go/bin
 go version
+git clone https://github.com/ipfs/go-ipfs
+cd go-ipfs
+make install
