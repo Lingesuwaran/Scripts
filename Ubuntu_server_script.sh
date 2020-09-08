a9ab704599629fcd5a229aba24119c37e5abd62b
Lingesuwaran <46320222+Lingesuwaran@users.noreply.github.com>
2020-09-08 18:18:05 +0530

    Update Ubuntu_server_script.sh

--- Ubuntu_server_script.sh
+++ Ubuntu_server_script.sh
@@ -1,8 +1,6 @@
 LA="vim make"
 
 apt install $LA
-mkdir downloads
-cd /home/$USER/downloads
 wget https://golang.org/dl/go1.15.1.linux-amd64.tar.gz
 tar -C /home/$USER -xzf go1.14.3.linux-amd64.tar.gz
 export PATH=$PATH:/home/$USER/go/bin
